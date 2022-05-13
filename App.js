d08f19649dd2758a333ffd0586ce5391ea3cba80
MJDJ <mjd.daif1995@gmail.com>
2022-05-13 22:30:48 +0200

    MDA commmit 13 may

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello this is my app</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
